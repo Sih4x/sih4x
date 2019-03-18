7d29edeb817e93850836d8465dd0d8d8284bbf6b
Sih4x <48694325+Sih4x@users.noreply.github.com>
2019-03-18 23:35:58 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+﻿#!/user/bin/env python
+import telebot
+
+
+token = '755294274:AAE9jBEwGon-_qf8cJj9IIKfdJyJ069_sLA' # Тестовый бот MainInvest
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет!', parse_mode="Html")
+
+
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
